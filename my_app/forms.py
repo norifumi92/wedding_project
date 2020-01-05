2a00735c19f771a409e031d80cef0763145690aa
LAPTOP-TERIQ5LU\norif <noristyle92@gmail.com>
2020-01-05 11:25:31 +0900

    updated wedding page and etnryform

--- my_app/forms.py
+++ my_app/forms.py
@@ -1,4 +1,6 @@
 from django import forms
 
 class EntryForm(forms.Form):
-    first_name = forms.CharField(label='First Name', max_length=50)
+    first_name = forms.CharField(label='First Name', max_length=50, widget=forms.TextInput(attrs={'placeholder': 'You must fill this',}))
+    middle_name = forms.CharField(label='Middle Name', max_length=50, required=False, widget=forms.TextInput(attrs={'placeholder': '',}))
+    last_name = forms.CharField(label='Last Name', max_length=50, required=True, widget=forms.TextInput(attrs={'placeholder': 'You must fill this',}))
