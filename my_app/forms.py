7202fdb7f15ee0e98864efc3f8d428c7cd2bd22d
LAPTOP-TERIQ5LU\norif <noristyle92@gmail.com>
2020-01-05 11:35:48 +0900

    fixed whatsapp and hid placeholder in wedding page

--- my_app/forms.py
+++ my_app/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class EntryForm(forms.Form):
-    first_name = forms.CharField(label='First Name', max_length=50, widget=forms.TextInput(attrs={'placeholder': 'You must fill this',}))
+    first_name = forms.CharField(label='First Name', max_length=50, widget=forms.TextInput(attrs={'placeholder': '',}))
     middle_name = forms.CharField(label='Middle Name', max_length=50, required=False, widget=forms.TextInput(attrs={'placeholder': '',}))
-    last_name = forms.CharField(label='Last Name', max_length=50, required=True, widget=forms.TextInput(attrs={'placeholder': 'You must fill this',}))
+    last_name = forms.CharField(label='Last Name', max_length=50, required=True, widget=forms.TextInput(attrs={'placeholder': '',}))
