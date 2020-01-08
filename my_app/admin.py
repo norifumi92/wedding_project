720053032135d8d0f42a246b6a9225d5a0d3f622
LAPTOP-TERIQ5LU\norif <noristyle92@gmail.com>
2020-01-09 01:18:28 +0900

    changed Admin title info

--- my_app/admin.py
+++ my_app/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 #import Model
 from my_app.models import Attendee
 
+#Configure Admin title
+admin.site.site_header = "Admin"
+admin.site.site_title = "Admin Portal"
+admin.site.index_title = "Welcome to Nori&Amira Wedding Admin Page!"
+
 # Register your models here.
 admin.site.register(Attendee)
